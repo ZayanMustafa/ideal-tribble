ed4f18ebb1cc583e7b200c4978aff0cd1276eb78
Zayyan Mustafa <mustafazayyan13@gmail.com>
2025-05-13 00:58:00 +0500

    Update code

--- index.js
+++ index.js
@@ -1,47 +1,13 @@
 
-
-// import express from "express"
-// import mongoose from "mongoose";
-// import vinOrderRouter from "./routes/order.js"
-// import cors from "cors"
-
-// const app = express();
-// const port = process.env.PORT || 5010;
-
-// //  Mongo db ke sath connect kar deya
-// try {
-    
-//     await mongoose.connect("mongodb+srv://muzammil:muzammil@cluster0.5ozicp4.mongodb.net/fussion?retryWrites=true&w=majority&appName=Cluster0")
-//     console.log("Mongo DB connected")
-
-// } catch (error) {
-    
-    
-//     console.log(error)
-// }
-
-
-// app.use(express.json());
-
-
-
-
-// app.listen(port, (err) => {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         console.log(`Server is running on port http://localhost:${port}`);
-//     }
-// });
-
-
-// app.use('/orders' , vinOrderRouter )
-
 // File : index.js
 import express from "express";
 import mongoose from "mongoose";
 import vinOrderRouter from "./routes/order.js";
 import cors from "cors";
+import dotenv from "dotenv";
+
+
+dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -67,7 +33,7 @@ app.use(express.json());
 
 
 // MongoDB connection
-mongoose.connect("mongodb+srv://muzammil:muzammil@cluster0.5ozicp4.mongodb.net/fussion?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://muzammil:muzammil@cluster0.5ozicp4.mongodb.net/fussion?retryWrites=true&w=majority&appName=Cluster0" )
   .then(
     
     () => console.log("MongoDB connected successfully")
